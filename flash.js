33eec830a01480717f65a5553896d1526381c4d9
Mallory Overton <malloryoverton3@gmail.com>
2017-12-06 09:31:27 -0700

    flaschard website

---
+++ flash.js
@@ -0,0 +1,9 @@
+
+// possible pushpin button for answers //
+  // $(document).ready(function(){
+  //   $('.target').pushpin({
+  //     top: 0,
+  //     bottom: 1000,
+  //     offset: 0
+  //   });
+  // });
